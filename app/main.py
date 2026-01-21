6b8f5ddd6bc15f50a375e670f451b226b932bc5e
cnc007 <cnccodecraftcronicles@gmail.com>
2026-01-21 14:16:04 +0530

    Day 3 progress: added FastAPI structure, user schemas, API stub

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from app.api.auth import router as auth_router
 
 app = FastAPI(title="Movie Booking Backend")
 
 @app.get("/")
 def home():
     return {"message": "Movie Booking Backend is running"}
+
+app.include_router(auth_router)
